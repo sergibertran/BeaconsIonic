cf63495d558fbb7f9d0fac63138e9f45373b30df
sergi.bertran <bigres24@gmail.com>
2021-03-23 13:07:11 +0100

    h

--- src/app/services/beacon-connection.service.ts
+++ src/app/services/beacon-connection.service.ts
@@ -1,20 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { BLE } from '@ionic-native/ble/ngx';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BeaconConnectionService {
+  devices:any[] = [];
 
-  constructor(private ble: BLE) {
+  constructor(private ble: BLE,private ngZone: NgZone) {
 
-    ble.startScan([]).subscribe(device => {
-      console.log(JSON.stringify(device));
+    }
 
+    doScan(){
+      this.ble.startScan([]).subscribe(device => {
+        console.log(JSON.stringify(device));
+        this.devices.push(device)
+      });
 
-    });
+    }
 
+    doStop(){
+      this.ble.stopScan();
 
-   }
+    }
 
+    getDevices(){
 
-}
+      return this.devices;
+    }
+
+  }
+
