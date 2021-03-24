204798b57151437e0d6ed9db953a9dc6da698a85
sergi.bertran <bigres24@gmail.com>
2021-03-24 10:11:09 +0100

    beacon info+constant search

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,31 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BeaconConnectionService } from '../services/beacon-connection.service';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  kontaktDevices: any[] = [];
+  sub1: any;
+  sub2: any;
+  meters: any;
 
-  constructor(private BeaconConnectionService: BeaconConnectionService) {}
+  constructor(private BeaconConnectionService: BeaconConnectionService) {
 
 
-  startScan(){
-
-
-  this.BeaconConnectionService.doScan();
 
   }
 
-  stopScan(){
+  ngOnInit() {
+    this.sub1 = interval(2000).subscribe((x) => {
+      console.log('2secons');
+      this.startScan();
+      this.scannedItems();
+    });
 
+
+ }
+
+  startScan() {
+    this.BeaconConnectionService.doScan();
+  }
+
+  stopScan() {
     this.BeaconConnectionService.doStop();
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe(); //x quina rao la de 6 no es para
+  }
+  scannedItems() {
+    this.kontaktDevices = this.BeaconConnectionService.getDevices();
+  }
 
-    }
+  viewItem(item) {
 
 
 
+    this.meters=Math.round(Math.pow(10,((-69 - (item.rssi))/(10 * 2))))
 
+
+    alert('RSSI: ' + item.rssi+ ' Concretamente a '+this.meters+' metros');
+  }
 
 
 }
--- src/app/services/beacon-connection.service.ts
+++ src/app/services/beacon-connection.service.ts
@@ -4,29 +4,55 @@ import { BLE } from '@ionic-native/ble/ngx';
   providedIn: 'root',
 })
 export class BeaconConnectionService {
-  devices:any[] = [];
+  devices: any[] = [];
 
-  constructor(private ble: BLE,private ngZone: NgZone) {
+  constructor(private ble: BLE, private ngZone: NgZone) {
 
-    }
 
-    doScan(){
-      this.ble.startScan([]).subscribe(device => {
-        console.log(JSON.stringify(device));
-        this.devices.push(device)
-      });
 
-    }
-
-    doStop(){
-      this.ble.stopScan();
-
-    }
-
-    getDevices(){
-
-      return this.devices;
-    }
 
   }
 
+  doScan() {
+    this.devices=[{
+      name: "TI SensorTag",
+      id: "BD922605-1B07-4D55-8D09-B66653E51BBA",
+      rssi: -79,
+      advertising: "aa"
+  },{
+    name: "TI SensorTag",
+    id: "BD922605-1B07-4D55-8D09-B66653E51BBA",
+    rssi: -86,
+    advertising: "aa"
+}];
+
+  /*
+    this.ble.startScan([]).subscribe((device) => {
+      console.log(JSON.stringify(device));
+      this.devices.push(device);
+    });
+    */
+  }
+
+  doStop() {
+    this.ble.stopScan();
+  }
+
+  getDevices() {
+
+    this.devices.sort((n1,n2) => {
+      if (n1.rssi < n2.rssi) {
+          return 1;
+      }
+
+      if (n1.rssi > n2.rssi) {
+          return -1;
+      }
+
+      return 0;
+  });
+
+
+    return this.devices;
+  }
+}
